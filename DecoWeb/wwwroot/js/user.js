8d92f50404202d3d0168b293d98e6fac9109a77e
LAPTOP-2GND2L99\NYU_M <nyu_miss@hotmail.com>
2024-01-16 16:48:18 +0700

    Completed user managment module

---
+++ DecoWeb/wwwroot/js/user.js
@@ -0,0 +1,30 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        "columns": [
+            { data: 'firstName', "width": "15%" },
+            { data: 'lastName', "width": "15%" },
+            { data: 'email', "width": "15%" },
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'hotel.name', "width": "15%" },
+            { data: 'role', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                                <a href="/admin/user/RoleManager?userId=${data}" class="btn btn-dark mx-2">
+                                    <i class="bi bi-pencil-square"></i>
+                                </a>
+                            </div>`
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
